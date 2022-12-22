b118f56ab497ba2788ab0d0d7bc566ce2dea826e
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-22 10:56:03 +0700

    Step 4 complete

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   console.log('node bin/brain-games.js');
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log('Hello ' + userName + '!');
 };
+
+export default greeting;
 
 // Wait for user's response.
