d265e60d840fb838a9a909c61c084644d3981f7a
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-30 13:51:41 +0700

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,6 +1,18 @@
 import { generateRandomNumber } from '../helper.js';
 import baseOfGames from '../index.js';
 
+const progressionForGame = (firstNumberProgression, progressionDifference, count = 10) => {
+  const arr = [];
+  for (
+    let i = firstNumberProgression;
+    i < progressionDifference * count;
+    i += progressionDifference
+  ) {
+    arr.push(i);
+  }
+  return arr;
+};
+
 const progression = () => {
   const gameQuestion = 'What number is missing in the progression?';
 
@@ -8,15 +20,8 @@ const progression = () => {
     console.log(gameQuestion);
     const firstNumberProgression = generateRandomNumber(5);
     const progressionDifference = generateRandomNumber(10);
-    const arr = [];
-    for (
-      let i = firstNumberProgression;
-      i < progressionDifference * 10;
-      i += progressionDifference
-    ) {
-      arr.push(i);
-    }
-    const randomIndex = Math.floor(Math.random() * arr.length);
+    const arr = progressionForGame(firstNumberProgression, progressionDifference);
+    const randomIndex = generateRandomNumber(arr.length - 1);
     const rightAnswer = arr[randomIndex];
     arr[randomIndex] = '..';
 
